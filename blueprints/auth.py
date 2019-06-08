819fad49be4828f140e79f55f6ecebe34d9c5801
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-06-08 17:57:18 -0300

    encerramento da aula de 08-06-2019

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,24 +1,120 @@
 
 import flask
+import ldap3
 
 blueprint = flask.Blueprint('auth', __name__)
 
 @blueprint.route('/sign-in', methods=[ 'GET' ])
 def get_sign_in():    
 
-    context = {
-        'page': 'sign-in',
-        'route': {
-            'is_public': True
-        },
-    }
+	context = {
+		'page': 'sign-in',
+		'route': {
+			'is_public': True
+		},
+	}
 
-    return flask.render_template('sign-in.html', context=context)
+	return flask.render_template('sign-in.html', context=context)
 
 @blueprint.route('/sign-in', methods=[ 'POST' ])
 def post_sign_in():
-    return ''
+
+	server = ldap3.server('ldap://127.0.0.1:389')
+	connection = ldap3.connection(
+		server,
+		'cn=admin,dc=dexter,dc=com,dc=br',
+		'4linux'
+	)
+
+	try:
+		connection.bind()
+
+	except:
+		flask.flash('Sem conexão com LDAP', 'danger')
+		return flask.redirect('/sign-up')
+
+
+	email = flask.requests.form['email']
+	password = flask.requests.form['password']
+
+	connection.search(
+		'uid={},dc=dexter,dc=com,dc-br'.format(email),
+		'(objectClass=person)',
+		attributes=[ 'userPassword' ]
+	)
+
+	try:
+		user = connection.entries[0]
+		saved_password = user.userPassword.value.decode()
+		if saved_password == password:
+			flask.session['email'] = email
+			return flask.redirect('/')
+	except:
+		flask.flash('Usuário não cadastrado', 'danger')
+		return flask.redirect('/sign-in')
+	return ''
 
 @blueprint.route('/sign-out', methods=[ 'POST' ])
 def post_sign_out():
-    return ''
+	del flask.session['email']
+	return flask.redirect('/sign-in')
+
+@blueprint.route('/sign-up', methods=[ 'GET' ])
+def get_sign_up():    
+
+	context = {
+		'page': 'sign-up',
+		'route': {
+			'is_public': True
+		},
+	}
+
+	return flask.render_template('sign-up.html', context=context)
+
+@blueprint.route('/sign-up', methods=[ 'POST' ])
+def post_sign_up():
+
+	server = ldap3.server('ldap://127.0.0.1:389')
+	connection = ldap3.connection(
+		server,
+		'cn=admin,dc=dexter,dc=com,dc=br',
+		'4linux'
+
+		)
+
+	try:
+		connection.bind()
+
+	except:
+		flask.flash('Sem conexão com LDAP', 'danger')
+		return flask.redirect('/sign-up')
+
+	name = flask.requests.form['name']
+	surname = flask.requests.form['surname']
+	email = flask.requests.form['email']
+	password = flask.requests.form['password']
+
+	object_class = [
+		'top'
+		'person'
+		'organizationalPerson',
+		'inet0rgPerson'
+	]
+
+	user = {
+		'cn' : name,
+		'sn' : surname,
+		'mail' : email,
+		'uid' : email,
+		'userPassword': password
+
+	}
+
+	cn = 'uid={},dc=dexter,dc=com,dc=br'.format(email)
+
+	if connection.add(cn, object_class, user):
+		return flask.redirect('/sign-in')
+
+	flask.flash('Erro ao cadastrar usuário', 'danger')
+	return flask.redirect('/sign-up')
+
--- blueprints/gitlab.py
+++ blueprints/gitlab.py
@@ -1,5 +1,9 @@
 
 import flask
+import requests
+
+DOMAIN = 'https://gitlab.com/api/v4'
+PROJECTS_URL = DOMAIN + '/projects?owned=true&private_token=bKB92xhzpUhf7hLJbJzx'
 
 blueprint = flask.Blueprint('gitlab', __name__)
 
@@ -12,10 +16,17 @@ def get_gitlab():
         'route': {
             'is_public': False
         },
+        'projects': requests.get(PROJECTS_URL).json()
     }
 
     return flask.render_template('gitlab.html', context=context)
 
-@blueprint.route('/gitlab', methods=[ 'POST' ])
-def post_gitlab():
-    pass
+@blueprint.route('/gitlab/<int:projectid>/commits', methods=[ 'GET' ])
+def get_commits(projectid):
+
+	ROUTE = '/projects/{}/reposição/commits?private_token=?bKB92xhzpUhf7hLJbJzx', format(projectid)
+
+	COMMITS_URL = DOMAIN + ROUTE
+
+	return requests.get(COMMITS_URL).text
+    
